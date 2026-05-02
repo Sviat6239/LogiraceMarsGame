67e65d6883e9d93fcb0d7558b11d77e1a698d6cb
Sviat6239 <sviatoslav6239@gmail.com>
2026-05-02 21:13:59 +0200

    added basic item classes

--- static/script/items.js
+++ static/script/items.js
@@ -1,2 +1,34 @@
 import { Item } from "./gameStats.js";
 
+class Food extends Item {
+    constructor(title, description, space, weight, feed, liquid, expirationDate) {
+        super(title, description, space, weight);
+        this.feed = feed;
+        this.liquid = liquid;
+        this.expirationDate = expirationDate;
+    }
+}
+
+class Tool extends Item {
+    constructor(title, description, space, weight, damage, durability, sharpness, hardness) {
+        super(title, description, space, weight);
+        this.damage = damage;
+        this.durability = durability;
+        this.sharpness = sharpness;
+        this.hardness = hardness;
+    }
+}
+
+class EmptyCan extends Item {
+    constructor(title, description, space, weight, volume) {
+        super(title, description, space, weight);
+        this.volume = volume;
+    }
+}
+
+class Canister extends Item {
+    constructor(title, description, space, weight, volume) {
+        super(title, description, space, weight);
+        this.volume = volume;
+    }
+}
