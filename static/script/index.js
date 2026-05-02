0970052dedd7c1a583a2904afef8715b989ff071
Sviat6239 <sviatoslav6239@gmail.com>
2026-05-02 14:46:50 +0200

    fixed oxygen logic added airPollution logic

--- static/script/index.js
+++ static/script/index.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const description = document.querySelector('.description');
     const buttons = document.querySelector('.moveBtn');
 
-    const inventoryBtn = document.querySelector('.inventory');
+    const inventoryBtn = document.querySelector('.inventoryBtn');
 
     player.location = mainState.modules[0];
 
@@ -35,9 +35,35 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
+    function allOxygen() {
+        mainState.oxygen = 0;
+
+        for (let i = 0; i < mainState.modules.length; i++) {
+            mainState.oxygen += mainState.modules[i].oxygen;
+        }
+    }
+
+    function allAirPollution() {
+        mainState.airPollution = 0;
+
+        for (let i = 0; i < mainState.modules.length; i++) {
+            mainState.airPollution += mainState.modules[i].airPollution;
+        }
+    }
+
     function tick() {
+        mainState.modules[8].oxygen += 0.01;
+        mainState.modules[9].oxygen += 0.01;
+
         player.location.oxygen -= 0.01;
-        console.log(player.location.oxygen);
+        player.location.airPollution += 0.01;
+
+        allOxygen();
+        allAirPollution();
+
+        console.log(mainState.oxygen);
+        console.log(mainState.airPollution);
+
         update();
     }
 
@@ -50,6 +76,7 @@ document.addEventListener("DOMContentLoaded", () => {
             alert("Inventory has been closed");
         }
     }
+
 
     update();
     setInterval(tick, 1000);
